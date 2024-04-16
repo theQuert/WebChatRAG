9e1c14873f1a32d9c6b761b931f17278fffa0bb2
theQuert <ga11004@cs.nccu.edu.tw>
2024-04-17 00:03:59 +0800

    Add gitignore and add the simple URL checker

--- src/app.py
+++ src/app.py
@@ -17,22 +17,24 @@ if "chat_history" not in st.session_state:
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
+if website_url is None or website_url == "":
+    st.info("Please enter a valid URL")
+else:
+    # user input
+    user_query = st.chat_input("Type your messages here...")
 
-# user input
-user_query = st.chat_input("Type your messages here...")
 
+    if user_query and user_query != "":
+        response = get_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
 
-if user_query and user_query != "":
-    response = get_response(user_query)
-    st.session_state.chat_history.append(HumanMessage(content=user_query))
-    st.session_state.chat_history.append(AIMessage(content=response))
-
-# conversation
-for message in st.session_state.chat_history:
-    if isinstance(message, AIMessage):
-        with st.chat_message("AI"):
-            st.write(message.content)
-    elif isinstance(message, HumanMessage):
-        with st.chat_message("Human"):
-            st.write(message.content)
+    # conversation
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
     
